a7e8cb5f20ccf23fc6f10280b61991d3bd982064
Manuel Benini <manuelbenini1905@gmail.com>
2022-09-15 15:25:38 +0200

    Consegne terminate, no bonus

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("I miei cibi preferiti!");
+
+string[] favoriteFoods = { "Pasta", "Carne", "Pollo", "Mottini", "Patatine" };
+
+Console.WriteLine("Lunghezza classifica: " + favoriteFoods.Length);
+
+Console.WriteLine("cibi in classifica: ");
+
+for (int i = 0; i < favoriteFoods.Length; i++)
+{
+    Console.WriteLine((i + 1)  + "=> " + favoriteFoods[i]);
+}
+
+Console.WriteLine("Primo cibo in classifica: " + favoriteFoods[0]);
+
+Console.WriteLine("Ultimo cibo in classifica: " + favoriteFoods[favoriteFoods.Length - 1]);
