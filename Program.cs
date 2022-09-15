1a3402b859f883ffbb5649871f8718e45d84835d
Manuel Benini <manuelbenini1905@gmail.com>
2022-09-15 15:49:40 +0200

    Bonus terminato

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("I miei cibi preferiti!");
 
-string[] favoriteFoods = { "Pasta", "Carne", "Pollo", "Mottini", "Patatine" };
+string[] favoriteFoods = { "Pasta", "Carne", "Pollo", "Mottini", "Patatine", "patate", "giacomo", "blabla" };
 
 Console.WriteLine("Lunghezza classifica: " + favoriteFoods.Length);
 
@@ -15,3 +15,14 @@ for (int i = 0; i < favoriteFoods.Length; i++)
 Console.WriteLine("Primo cibo in classifica: " + favoriteFoods[0]);
 
 Console.WriteLine("Ultimo cibo in classifica: " + favoriteFoods[favoriteFoods.Length - 1]);
+
+double median = (double)favoriteFoods.Length / 2;
+
+if (median % 2 == 0)
+{
+    Console.WriteLine("2 Cibi in posizione mediana: " + favoriteFoods[(int)median - 1] + ", " + favoriteFoods[(int)median]);
+}
+else
+{
+    Console.WriteLine("3 Cibi in posizione mediana: " + favoriteFoods[(int)median- 1] + ", " + favoriteFoods[(int)median] + ", " + favoriteFoods[(int)median + 1]);
+}
