650895364376d1d00879f14bf25f5c48bf302043
Manuel Benini <manuelbenini1905@gmail.com>
2022-09-15 16:05:47 +0200

    Fix bonus alla mediana dispari

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("I miei cibi preferiti!");
 
-string[] favoriteFoods = { "Pasta", "Carne", "Pollo", "Mottini", "Patatine", "patate", "giacomo", "blabla" };
+string[] favoriteFoods = { "Pasta", "Carne", "Pollo", "Mottini", "Patatine"};
 
 Console.WriteLine("Lunghezza classifica: " + favoriteFoods.Length);
 
@@ -18,11 +18,13 @@ Console.WriteLine("Ultimo cibo in classifica: " + favoriteFoods[favoriteFoods.Le
 
 double median = (double)favoriteFoods.Length / 2;
 
+Console.WriteLine((int)median);
+
 if (median % 2 == 0)
 {
     Console.WriteLine("2 Cibi in posizione mediana: " + favoriteFoods[(int)median - 1] + ", " + favoriteFoods[(int)median]);
 }
 else
 {
-    Console.WriteLine("3 Cibi in posizione mediana: " + favoriteFoods[(int)median- 1] + ", " + favoriteFoods[(int)median] + ", " + favoriteFoods[(int)median + 1]);
+    Console.WriteLine("Cibo in posizione mediana: " + favoriteFoods[(int)median]);
 }
